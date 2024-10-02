d4ad8f4780b0fd26c24937a6c64e98ca7048bd86
YenaLey <yena.e121@gmail.com>
2024-10-02 16:14:29 +0900

    Delete: auth api 관련 내용 모두 삭제

--- app/api/swagger/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import swaggerDocument from "../../swagger.json"; // 루트 경로에 있는 swagger.json 파일
-
-// GET 요청을 처리하는 named export
-export async function GET(req: NextRequest) {
-  return NextResponse.json(swaggerDocument);
-}
--- app/apidocs/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function SwaggerLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="apidocs-container">{children}</div>;
-}
