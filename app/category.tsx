742066b658b71f1b9d2ebe72a7d8746a55d5b74c
YenaLey <yena.e121@gmail.com>
2024-10-03 19:04:03 +0900

    Feat: category 서버 사이드에서 데이터 패칭으로 수정

---
+++ app/category.tsx
@@ -0,0 +1,40 @@
+"use client"; // 클라이언트 컴포넌트로 지정
+
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { CategoryType } from "types/types";
+
+type CategoryProps = {
+  categories: CategoryType[] | null;
+};
+
+export default function Category({ categories }: CategoryProps) {
+  const router = useRouter();
+
+  const fetchRecipes = (category: string) => {
+    router.push(`/menu?category=${category}`);
+  };
+
+  return (
+    <div className="home-container">
+      <h1>Choose a Category!</h1>
+      {categories && categories.length > 0 ? (
+        categories.map((category) => (
+          <div key={category.idCategory}>
+            <p onClick={() => fetchRecipes(category.strCategory)}>
+              {category.strCategory}
+            </p>
+            <Image
+              src={category.strCategoryThumb}
+              alt={category.strCategory}
+              width={150}
+              height={100}
+            />
+          </div>
+        ))
+      ) : (
+        <p>Failed to load categories. Please try again later.</p>
+      )}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,19 @@
-"use client";
+import Category from "./category";
 
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { Category } from "types/types";
+async function getCategories() {
+  try {
+    const res = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/categories.php"
+    );
+    const data = await res.json();
+    return data.categories || [];
+  } catch (error) {
+    console.error("Failed to fetch categories", error);
+    return [];
+  }
+}
 
-export default function Home() {
-  const router = useRouter();
-  const [categories, setCategories] = useState<Category[] | null>(null);
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const res = await fetch(
-          "https://www.themealdb.com/api/json/v1/1/categories.php"
-        );
-        const data = await res.json();
-        setCategories(data.categories);
-      } catch (error) {
-        console.error("Failed to fetch categories", error);
-      }
-    };
-
-    fetchCategories();
-  }, []);
-
-  const fetchRecipes = async (category: string) => {
-    router.push(`/menu?category=${category}`);
-  };
-
-  return (
-    <div className="home-container">
-      <h1>Choose a Category!</h1>
-      {categories && categories.length > 0 ? (
-        categories.map((category) => (
-          <div key={category.idCategory}>
-            <p onClick={() => fetchRecipes(category.strCategory)}>
-              {category.strCategory}
-            </p>
-            <Image
-              src={category.strCategoryThumb}
-              alt={category.strCategory}
-              width={150}
-              height={100}
-            />
-          </div>
-        ))
-      ) : (
-        <p>Loading categories...</p>
-      )}
-    </div>
-  );
+export default async function Home() {
+  const categories = await getCategories();
+  return <Category categories={categories} />;
 }
